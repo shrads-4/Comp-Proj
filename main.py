b24d6b5c33beeaa79e81f10777fbc0978b08fac8
Shraddha <shradp21@gmail.com>
2020-06-10 10:00:42 +0530

    screen.fill

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
 # game loop
 running = True
 while running:
+    #background temp color
+    screen.fill((255,163,249))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    pygame.display.update()
 
     #title and icon
     pygame.display.set_caption("NameOfGame")
     icon=pygame.image.load('run.png')
     pygame.display.set_icon(icon)
+
