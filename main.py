c4022d6114aa740e429b1e2f160067964915bed4
Shraddha <shradp21@gmail.com>
2020-06-10 18:10:45 +0530

    added png files
    added background, replaced run.png with vampire.png as icon

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import pygame
 pygame.init()
 
 # creating the screen
-screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
+screen = pygame.display.set_mode((800, 640), pygame.RESIZABLE)
+
+#background
+background=pygame.image.load('background.png')
 
 # game loop
 running = True
 while running:
-    #background temp color
-    screen.fill((255,163,249))
+
+    #background
+    screen.blit(background,(0,0))
+
+    #title and icon
+    pygame.display.set_caption("NameOfGame")
+    icon=pygame.image.load('vampire.png')
+    pygame.display.set_icon(icon)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,8 +26,5 @@ while running:
 
     pygame.display.update()
 
-    #title and icon
-    pygame.display.set_caption("NameOfGame")
-    icon=pygame.image.load('run.png')
-    pygame.display.set_icon(icon)
+    
 
