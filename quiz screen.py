d26aa6b1af2aaa9b12786658e23c7d05e8ea1c93
Aditi <adbhat104@gmail.com>
2020-06-14 11:24:44 +0530

    Create quiz screen.py

---
+++ quiz screen.py
@@ -0,0 +1,42 @@
+import pygame,sys,pygbutton,time
+
+pygame.init()  
+screen = pygame.display.set_mode((700,600))
+pygame.display.set_caption("<name/topic>")
+pygame.display.flip()
+
+def quiz():
+    i=0
+    question = pygbutton.PygButton((200,80,300,80),"<question>")
+    answer1 = pygbutton.PygButton((250,260,200,30),"option1")
+    answer2 = pygbutton.PygButton((250,300,200,30),"option2")
+    answer3 = pygbutton.PygButton((250,340,200,30),"option3")
+    answer4 = pygbutton.PygButton((250,380,200,30),"option4")
+
+    question.draw(screen)
+    answer1.draw(screen)
+    answer2.draw(screen)
+    answer3.draw(screen)
+    answer4.draw(screen)
+    buttonEvent = answer1.handleEvent(event)
+    buttonEvent2= answer2.handleEvent(event)
+    buttonEvent3= answer3.handleEvent(event)
+    buttonEvent4= answer4.handleEvent(event)
+
+    question.draw(screen)
+    answer1.draw(screen)
+    answer2.draw(screen)
+    answer3.draw(screen)
+    answer4.draw(screen)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+            sys.exit()
+
+    quiz()
+
+    pygame.display.update()
+    
